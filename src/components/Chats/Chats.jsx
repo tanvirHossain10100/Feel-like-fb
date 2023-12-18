21ab3e64ce085cb7872a4a0e0b4c436d09da02d2
tanvirHossain10100 <tanvirhossan528@gmail.com>
2023-12-18 21:09:32 +0600

    tyring to solve vercel hosting bughs

--- src/components/Chats/Chats.jsx
+++ src/components/Chats/Chats.jsx
@@ -1,10 +1,41 @@
 import { Loading } from "../../Loading/Loading";
+import { Chat } from "../Chat/Chat";
 import "./Chats.css";
+import { SlNote } from "react-icons/sl";
+import { BsThreeDots } from "react-icons/bs";
+import { useFetch } from "../../customHooks/useFetch/UseFetch";
 
 export const Chats = () => {
+  const [data] = useFetch();
+
   return (
     <>
-      <Loading></Loading>
+      {!data[0]?.cell ? (
+        <Loading></Loading>
+      ) : (
+        <div>
+          <div className="chateHeaderConainer">
+            <div className=" chatsHeaser">
+              <h2>Chats</h2>
+
+              <div className="chatHeaderIcon">
+                <SlNote />
+                <BsThreeDots />
+              </div>
+            </div>
+            <input type="text" placeholder="Search messenger" />
+            <div className="cheatHeaderToggle">
+              <button>Inbox</button>
+              <button>Communities</button>
+            </div>
+          </div>
+          <div className="chatsContainer">
+            {data.map((chat, index) => (
+              <Chat key={index} chat={chat}></Chat>
+            ))}
+          </div>
+        </div>
+      )}
     </>
   );
 };
